f70b6050448e23aa7c1ed9a57b62b57a4d124d40
Paul Johnson <paul@snuberry-software.co.uk>
2015-03-10 00:06:02 +0000

    Initial import

---
+++ RelativeLayoutButton/RelativeLayoutButton/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace RelativeLayoutButton
+{
+    [Activity(Label = "RelativeLayoutButton", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            var button1 = new RelativeLayoutButton(this, Resource.Id.button1);
+            button1.Click += delegate
+            {
+                Toast.MakeText(this, "Relative layout button clicked", ToastLength.Long).Show();
+            };
+
+            var button2 = new RelativeLayoutButton(this, Resource.Id.button2);
+            button2.SetText(Resource.Id.test_button_text1, "Change");
+            button2.SetText(Resource.Id.test_button_text2, "text");
+
+            var button3 = new RelativeLayoutButton(this, Resource.Id.button3);
+            button3.SetText(Resource.Id.test_button_text1, "Change");
+            button3.SetText(Resource.Id.test_button_text2, "image");
+            button3.SetImageResource(Resource.Id.test_button_image, Android.Resource.Drawable.StarBigOn);
+
+        }
+    }
+}
+
+
---
+++ RelativeLayoutButton/RelativeLayoutButton/RelativeLayoutButton.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Views;
+using Android.Widget;
+using Android.Graphics.Drawables;
+
+namespace RelativeLayoutButton
+{
+    public class RelativeLayoutButton : RelativeLayout
+    {
+        public RelativeLayoutButton(Context context, int id)
+            : base(context)
+        {
+            if (!(context is Activity))
+                return;
+
+            var v = ((Activity)context).FindViewById(id);
+
+            if (!(v is RelativeLayout))
+                return;
+
+            var layout = (RelativeLayout)v;
+            var layparams = layout.LayoutParameters;
+            this.LayoutParameters = layparams;
+            var btn = new Button(context);
+            this.SetBackgroundDrawable(btn.Background);
+            while (layout.ChildCount > 0)
+            {
+                var vchild = layout.GetChildAt(0);
+                layout.RemoveViewAt(0);
+                this.AddView(vchild);
+                if (vchild is TextView)
+                    ((TextView)vchild).SetTextColor(btn.TextColors);
+                vchild.Clickable = vchild.Focusable = true;
+                vchild.FocusableInTouchMode = false;
+
+                var vp = (ViewGroup)layout.Parent;
+                if (vp != null)
+                {
+                    var index = vp.IndexOfChild(layout);
+                    vp.RemoveView(layout);
+                    vp.AddView(this, index);
+                }
+                this.Id = id;
+            }
+        }
+
+        public void SetText(int id, string text)
+        {
+            View v = FindViewById(id);
+            if (v != null && v is TextView)
+                ((TextView)v).Text = text;
+        }
+
+        public void SetImageDrawable(int id, Drawable drawable)
+        {
+            View v = FindViewById(id);
+            if (v != null && v is ImageView)
+                ((ImageView)v).SetImageDrawable(drawable);
+        }
+
+        public void SetImageResource(int id, int resourceid)
+        {
+            View v = FindViewById(id);
+            if (v != null && v is ImageView)
+                ((ImageView)v).SetImageResource(resourceid);
+        }
+    }
+}
+
